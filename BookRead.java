59092f0b9ae5f62d2dc77177dd72974529e84d9a
jbutterworthUA <148112741+jbutterworthUA@users.noreply.github.com>
2024-09-25 18:30:07 -0700

    Update BookRead.java
    Adding comments.

--- BookRead.java
+++ BookRead.java
@@ -1,15 +1,29 @@
 public class BookRead {
     private Book book;
-    private boolean isRead = false;
+    private boolean isRead;
 
+    /**
+    * Contructor for the BookRead class. Bundles previous Book class
+    * with a new attribute to mark its read status.
+    * @param Book. The Book object we will be initializing our instance variable with.
+    */
     public BookRead(Book myBook) {
         this.book = myBook;
+        isRead = false;
     }
 
+    /**
+    * Setter method which will be used to update a BookRead object's read status.
+    * Read status will only be changed to true, as it is initialized to false in constructor.
+    */
     public void setRead() {
         this.isRead = true;
     }
 
+    /**
+    * Getter method which will be used to access the instance variable of our Book object.
+    * Object will not act as escaping reference, as Book class is final and immutable.
+    */
     public Book getBook() {
         return this.book;
     }
