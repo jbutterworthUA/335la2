c01ba2f8d6c972f930f143146b4437eb3c9d4b1b
jbutterworthUA <148112741+jbutterworthUA@users.noreply.github.com>
2024-09-25 19:06:51 -0700

    Update MyLibrary.java
    Adding comments.

--- MyLibrary.java
+++ MyLibrary.java
@@ -1,8 +1,13 @@
 /*
- * Authors: Jason Butterworth.
- * Test line.
+ * Authors: Jason Butterworth (jbutterworth) and Dylan Carothers (NETID).
+ * Class Purpose: Running a text-based UI allowing user to get or add information
+ * to a personal virtual library.
  */
 
-public class MyLibrary {
-
-}
+  public class MyLibrary {
+    
+    public static void Main(String[] args) {
+        
+    }
+    
+  }
